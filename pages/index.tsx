d48368ed74bbb34ae62d87d7d8bb61ff418adbcd
nayrr <nayrr@nayrrs-MacBook-Air.local>
2023-04-18 19:58:05 +0700

    fix: change position claim

--- pages/index.tsx
+++ pages/index.tsx
@@ -396,6 +396,17 @@ const Claim: NextPage = () => {
                           }}
                         />
                       ))}
+                    <div className="h-full justify-center items-center">
+                      <button className="cursor-pointer hover:brightness-105 active:brightness-95 hover:-translate-y-px disabled:grayscale disabled:cursor-not-allowed disabled:hover:translate-y-0 disabled:hover:brightness-100 disabled:opacity-70"
+                      disabled={
+                        getSelected().length === 0 || getSelected().length % 2 !== 0
+                      }
+                      onClick={() => handleClaim()}
+                      // loading={isLoading}
+                      >
+                    <img src="/assets/MOBILE/CLAIM-NOW.png" alt="Claim Button" className="h-40" />
+                    </button>
+                    </div>
                     </div>
                   )}
                 </div>
@@ -405,38 +416,6 @@ const Claim: NextPage = () => {
                 >
                   <ArrowRightIcon className="mx-auto w-14 h-14" />
                 </button>
-              </div>
-
-              {/* <div className="flex items-center justify-between space-x-4 w-full h-1/4">
-                <Button
-                  className="!w-44 h-14 text-lg"
-                  onClick={() => handleSelectAll()}
-                  variant="secondary"
-                >
-                  {!selectedAll && "Select All"}
-                  {selectedAll && "Unselect All"}
-                </Button>
-                <Button
-                  className="!w-56 h-14 text-lg"
-                  disabled={
-                    getSelected().length === 0 || getSelected().length % 2 !== 0
-                  }
-                  onClick={() => handleClaim()}
-                  loading={isLoading}
-                >
-                  Claim Selected ({getSelected().length})
-                </Button>
-              </div> */}
-              <div className="h-full justify-center items-center">
-                <button className="cursor-pointer hover:brightness-105 active:brightness-95 hover:-translate-y-px disabled:grayscale disabled:cursor-not-allowed disabled:hover:translate-y-0 disabled:hover:brightness-100 disabled:opacity-70"
-                disabled={
-                  getSelected().length === 0 || getSelected().length % 2 !== 0
-                }
-                onClick={() => handleClaim()}
-                // loading={isLoading}
-                >
-              <img src="/assets/MOBILE/CLAIM-NOW.png" alt="Claim Button" className="h-40" />
-              </button>
               </div>
             </div>
             {/* </Card> */}
