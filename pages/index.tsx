356bdd8d4c7f6d85dcbb491a2f6276b0445b45f0
nayrr <nayrr@nayrrs-MacBook-Air.local>
2023-04-18 19:50:18 +0700

    add: claim button

--- pages/index.tsx
+++ pages/index.tsx
@@ -407,7 +407,7 @@ const Claim: NextPage = () => {
                 </button>
               </div>
 
-              <div className="flex items-center justify-between space-x-4 w-full h-1/4">
+              {/* <div className="flex items-center justify-between space-x-4 w-full h-1/4">
                 <Button
                   className="!w-44 h-14 text-lg"
                   onClick={() => handleSelectAll()}
@@ -426,6 +426,17 @@ const Claim: NextPage = () => {
                 >
                   Claim Selected ({getSelected().length})
                 </Button>
+              </div> */}
+              <div className="h-full justify-center items-center">
+                <button className="cursor-pointer hover:brightness-105 active:brightness-95 hover:-translate-y-px disabled:grayscale disabled:cursor-not-allowed disabled:hover:translate-y-0 disabled:hover:brightness-100 disabled:opacity-70"
+                disabled={
+                  getSelected().length === 0 || getSelected().length % 2 !== 0
+                }
+                onClick={() => handleClaim()}
+                // loading={isLoading}
+                >
+              <img src="/assets/MOBILE/CLAIM-NOW.png" alt="Claim Button" className="h-40" />
+              </button>
               </div>
             </div>
             {/* </Card> */}
